62a3b046a95ab34d0076d0aa87c1d5d585ac94e0
Heide-00 <gulaslan93@gmail.com>
2025-07-15 12:45:07 +0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        /*Grocery grocery=new Grocery();
+        grocery.startGrocery();*/
+
+       MobilePhone mobilePhone=new MobilePhone("2342342342",new ArrayList<>());
+       mobilePhone.addNewContact(new Contact("emrah","2342342342"));
+       mobilePhone.addNewContact(new Contact("emrah","2342342342"));
+       mobilePhone.addNewContact(new Contact("emrah","2342342342"));
+       mobilePhone.addNewContact(new Contact("fhjklllşfj","23u2342342"));
+       mobilePhone.printContacts();
     }
 }
